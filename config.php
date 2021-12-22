28a2f720fccf395aca870aa9fca64bf8513af2a5
David Ta <david.ta@saylor.org>
2021-12-22 13:26:24 -0600

    Update config file

--- config.php
+++ config.php
@@ -28,3 +28,12 @@ return_once(__DIR__ . '/lib.php');
 
 $THEME->name = 'sayloru';
 $THEME->parents = ['boost', 'classic'];
+
+// A dock is a way to take blocks out of the page and put them in a persistent floating area on the side of the page. Boost
+// does not support a dock so we won't either - but look at bootstrapbase for an example of a theme with a dock.
+$THEME->enable_dock = false;
+
+// This is a list of blocks that are required to exist on all pages for this theme to function correctly. For example
+// bootstrap base requires the settings and navigation blocks because otherwise there would be no way to navigate to all the
+// pages in Moodle. Boost does not require these blocks because it provides other ways to navigate built into the theme.
+$THEME->requiredblocks = '';   
